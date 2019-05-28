79f72c8589725916245e2f7e603313db010dc740
Abu Yousuf <ayf.tutorial@gmail.com>
2019-05-28 14:43:47 +0530

    Create a package of in.embryo.system and changes happen in App class

--- PropertiesFileRead/src/main/java/in/embryo/PropertiesFileRead/App.java
+++ PropertiesFileRead/src/main/java/in/embryo/PropertiesFileRead/App.java
@@ -2,6 +2,6 @@ package in.embryo.PropertiesFileRead;
 
 public class App {
 	public static void main(String[] args) {
-		
+		System.out.println("this is the testing for git");
 	}
 }
---
+++ PropertiesFileRead/src/main/java/in/embryo/system/Simple.java
@@ -0,0 +1,5 @@
+package in.embryo.system;
+
+public class Simple {
+
+}
